13e61f856e6da6cab94ea1774de0d1882187a78b
simonebastasin <104850132+simonebastasin@users.noreply.github.com>
2023-05-04 18:49:10 +0200

    HW1

---
+++ Homework1/src/Nano.java
@@ -0,0 +1,10 @@
+
+public class Nano extends Tipo {
+	
+	//costruttore di default
+	Nano() {
+		attack = 2;
+		defence = 5;
+	}
+
+}
---
+++ Homework1/src/Tipo.java
@@ -0,0 +1,13 @@
+
+public class Tipo {
+
+	int attack;
+	int defence;
+	
+	//costruttore di default
+	Tipo() {
+		attack = 0;
+		defence = 0;
+	}
+	
+}
