9549e43f7eae38b6b3a1776523e58c3f2cef53b1
simonebastasin <104850132+simonebastasin@users.noreply.github.com>
2023-05-04 18:50:53 +0200

    HW2 SAFe TestDoc files

---
+++ Homework2/SAFe TestDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CollectionAdapterTest"},{"p":"<Unnamed>","l":"ListAdapterTest"},{"p":"<Unnamed>","l":"MapAdapterTest"},{"p":"<Unnamed>","l":"SetAdapterTest"}]
